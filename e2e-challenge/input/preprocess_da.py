272c8f8c347060d0f0e8576ca98e0a210072af56
Sharath T S <ts.sharath18@gmail.com>
2018-03-01 23:28:57 -0800

    Writing context parameter file update

--- e2e-challenge/input/preprocess_da.py
+++ e2e-challenge/input/preprocess_da.py
@@ -16,4 +16,5 @@ da_and_text = data[[mr_cols] + [text_cols] + [voice]]
 context = data[param_cols]
 
 da_and_text.to_csv(file_name.split('.')[0] + "_voices_trainset_mtv.csv", index=False)
-context.to_csv(file_name.split('.')[0] + "_voices_context_mtv.txt", index=False)#, sep=' ')#, header=False)
+#context.to_csv(file_name.split('.')[0] + "_voices_context_mtv.txt", index=False)#, sep=' ')#, header=False)
+context.to_csv(file_name.split('.')[0] + "_voices_context_personality.txt", index=False, sep=' ', header=False)
